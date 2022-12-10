2be1c7f073cfc3da739e0916e6283c9fd64e5b9e
satoshidg <satoshidg3104@gmail.com>
2022-12-09 22:07:57 -0800

    transferring code to pc

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ def main():
         min_detection_confidence=0.5
     )
 
+    hands_logged = False
+    face_logged = False
+
     while cap.isOpened():
         success, image = cap.read()
         if not success:
@@ -42,8 +45,24 @@ def main():
             # Draw the hand annotations on the image.
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if face_results.detections:
+                if not face_logged:
+                    print(face_results.detections)
+                    face_logged = True
+                for detection in face_results.detections:
+                    mp_drawing.draw_detection(image, detection)
+
             if hand_results.multi_hand_landmarks:
+                if not hands_logged:
+                    print(hand_results.multi_hand_landmarks)
+                    hands_logged = True
+
                 for hand_landmarks in hand_results.multi_hand_landmarks:
+                    temp = hand_landmarks.landmark[0]
+                    # print(dir(temp))
+                    # print(type(temp))
+                    # print(temp.x)
                     mp_drawing.draw_landmarks(
                         image,
                         hand_landmarks,
@@ -51,22 +70,35 @@ def main():
                         mp_drawing_styles.get_default_hand_landmarks_style(),
                         mp_drawing_styles.get_default_hand_connections_style())
 
-            if face_results.detections:
-                for detection in face_results.detections:
-                    mp_drawing.draw_detection(image, detection)
-
             # Flip the image horizontally for a selfie-view display.
             cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+
+        if not face_results.detections or not hand_results.multi_hand_landmarks:
+            continue
+        
+        # List of face boundaries each element should be (x_left, x_right, y_top, y_bottom)
+        faces = list()
+        for detection in face_results.detections:
+            box = detection.location_data.relative_bounding_box
+            faces.append((box.xmin, box.xmin + box.width, box.ymin, box.ymin + box.height))
+        print("faces:", faces)
+
+
+
         if cv2.waitKey(5) & 0xFF == 27:
             break
 
     cap.release()
     hands_detector.release()
 
+def debugPrint(message: str):
+    if DEBUG:
+        print(message)
+
 def alertUser():
     play("sound.mp3", False)
 
 if __name__ == '__main__':
-    if sys.argv == "debug":
+    if len(sys.argv) > 1 and sys.argv[1] == "debug":
         DEBUG = True
     main()
